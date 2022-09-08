1d312547ecb0442c67703592327eda853b637ebe
Jason Martinez <jasonmar08@gmail.com>
2022-09-08 01:28:23 -0400

    created controllers and routes for barber services

--- controllers/BarberController.js
+++ controllers/BarberController.js
@@ -1,4 +1,4 @@
-const { Barber } = require('../models')
+const { Barber, Appointment, Service } = require('../models')
 
 const getAllBarbers = async (req, res) => {
   try {
@@ -56,10 +56,83 @@ const deleteBarber = async (req, res) => {
   }
 }
 
+const getAllServices = async (req, res) => {
+  try {
+    let services = await Service.findAll()
+    res.send(services)
+  } catch (error) {
+    throw error
+  }
+}
+
+const getBarberServices = async (req, res) => {
+  try {
+    let barberId = parseInt(req.params.barber_id)
+    let barberServices = await Service.findAll({
+      where: { barberId: barberId }
+    })
+    res.send(barberServices)
+  } catch (error) {
+    throw error
+  }
+}
+
+const createNewService = async (req, res) => {
+  try {
+    let newService = await Service.create({ ...req.body })
+    res.send(newService)
+  } catch (error) {
+    throw error
+  }
+}
+
+const updateService = async (req, res) => {
+  try {
+    let serviceId = parseInt(req.params.service_id)
+    let updatedService = await Service.update(req.body, {
+      where: { id: serviceId },
+      returning: true
+    })
+    res.send(updatedService)
+  } catch (error) {
+    throw error
+  }
+}
+
+const deleteService = async (req, res) => {
+  try {
+    let serviceId = parseInt(req.params.service_id)
+    await Service.destroy({
+      where: { id: serviceId }
+    })
+    res.send({ msg: `Deleted barber service with id of ${serviceId}` })
+  } catch (error) {
+    throw error
+  }
+}
+
+const getBarberAppointments = async (req, res) => {
+  try {
+    let barberId = parseInt(req.params.barber_id)
+    let barberAppointments = await Appointment.findAll({
+      where: { barberId: barberId }
+    })
+    res.send(barberAppointments)
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
   getAllBarbers,
   getBarberById,
   // createNewBarber,
   updateBarber,
-  deleteBarber
+  deleteBarber,
+  getAllServices,
+  getBarberServices,
+  createNewService,
+  updateService,
+  deleteService,
+  getBarberAppointments
 }
--- routes/BarberRouter.js
+++ routes/BarberRouter.js
@@ -22,5 +22,11 @@ Router.delete(
   middleware.verifyToken,
   barberController.deleteBarber
 )
+Router.get('/appointments/:barber_id', barberController.getBarberAppointments)
+Router.get('/services/all', barberController.getAllServices)
+Router.get('/services/:barber_id', barberController.getBarberServices)
+Router.post('/services', barberController.createNewService)
+Router.put('/services/:service_id', barberController.updateService)
+Router.delete('/services/:service_id', barberController.deleteService)
 
 module.exports = Router
